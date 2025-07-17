06e429e486008835afa29af404ff9854858f769c
Ivan <arouba61@gmail.com>
2025-07-17 21:56:04 +0300

    Uncomplete Observable support

---
+++ Runtime/Observable.cs
@@ -0,0 +1,42 @@
+namespace Utilities
+{
+    // [Serializable]
+    public class Observable<T> : Observable
+    {
+        public delegate void ObservableValueChangedDelegate(T oldValue, T newValue);
+
+        public event ObservableValueChangedDelegate ValueChanged = delegate { };
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                var oldVal = _value;
+                _value = value;
+                ValueChanged?.Invoke(oldVal, _value);
+            }
+        }
+
+        // [SerializeField]
+        private T _value;
+
+        public Observable()
+        {
+            _value = default;
+        }
+        
+        public Observable(T value)
+        {
+            _value = value;
+        }
+
+
+        public override object GetValue() => Value;
+    }
+
+    public abstract class Observable
+    {
+        public abstract object GetValue();
+    }
+}
